d19d0d8080c036c8d000d37f601d5bb72d7a1b6a
Cong-Tien <letien18102001@gmail.com>
2023-11-05 23:19:04 +0700

    fix redirect

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as express from 'express';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.use(express.static('.'));
   const config = new DocumentBuilder().setTitle('Swagger').setVersion('1000').build();
   const document = SwaggerModule.createDocument(app,config);
   SwaggerModule.setup("/api/v1/swagger",app,document)
